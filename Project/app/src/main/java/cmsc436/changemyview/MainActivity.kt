2526b429107019930e016b528be1b1f13e1606a2
QuantumRetrofit <jaws540@gmail.com>
2020-11-28 16:45:12 -0500

    Starter project files

---
+++ Project/app/src/main/java/cmsc436/changemyview/MainActivity.kt
@@ -0,0 +1,11 @@
+package cmsc436.changemyview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
