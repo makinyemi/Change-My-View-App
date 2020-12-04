2adf9aba657d8bbd4bc3d63873e9008d4c1bf013
QuantumRetrofit <jaws540@gmail.com>
2020-12-04 12:59:42 -0500

    Initial firebase code

--- Project/app/src/main/java/cmsc436/changemyview/MainActivity.kt
+++ Project/app/src/main/java/cmsc436/changemyview/MainActivity.kt
@@ -2,10 +2,19 @@ package cmsc436.changemyview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home)
+
+        //Database.pushChat("exampleDebateID", "exampleUID", "Lol, idk whats happening")
+        Database.pushDebateTopic(
+            "Example Debate Topic",
+            listOf("Question 1", "Question 2", "Question 3", "Question 4", "Question 5"),
+            3600
+        )
     }
 }
