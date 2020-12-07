194469f07efae424d566b782a5848346bf794295
Michael Akinyemi <michael@outstrip.biz>
2020-12-07 11:46:13 -0500

    Database connected but progamatically adding from db isnt working

--- Project/app/src/main/java/cmsc436/changemyview/TopicActivity.kt
+++ Project/app/src/main/java/cmsc436/changemyview/TopicActivity.kt
@@ -12,7 +12,7 @@ import com.google.firebase.database.*
 class TopicActivity : AppCompatActivity() {
     private var recyclerView: RecyclerView? = null
     private var gridLayoutManager: GridLayoutManager? = null
-    private var arrayList:ArrayList<TopicItem> ? = null
+    private var mTopicList: ArrayList<TopicItem>? = null
     private var topicItemAdapters: TopicItemAdapters? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,44 +24,33 @@ class TopicActivity : AppCompatActivity() {
             GridLayoutManager(applicationContext, 2, GridLayoutManager.VERTICAL, false)
         recyclerView?.layoutManager = gridLayoutManager
         recyclerView?.setHasFixedSize(true)
-
-
-
-        fetchTopics()
-    }
-
-    private fun fetchTopics() {
-        arrayList = ArrayList()
+        mTopicList = ArrayList()
 
         Database.debates.addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                mTopicList!!.clear()
+                snapshot.children.forEach {
+                    val title = it.child("title").value.toString()
+                    Log.d("NewMessage", title)
+                    mTopicList!!.add(TopicItem(title))
+
+                }
+            }
+
             override fun onCancelled(error: DatabaseError) {
                 TODO("Not yet implemented")
             }
-
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val title = snapshot.getValue(DebateTopic::class.java)
-                Log.d("NewMessage", snapshot.toString())
-                if (title != null) {
-                    arrayList!!.add(TopicItem(title.title))
-                }
-            }
         })
-        topicItemAdapters = TopicItemAdapters(applicationContext, arrayList!!)
+        Log.d("Size of list: ", mTopicList!!.size.toString())
+        topicItemAdapters = TopicItemAdapters(applicationContext, mTopicList!!)
         recyclerView?.adapter = topicItemAdapters
+    }
+
+    private fun fetchTopics() {
+
+
 
     }
 
-    private fun setTopicsToList() : ArrayList<TopicItem>{
-
-        var items:ArrayList<TopicItem> = ArrayList()
-
-        items.add(TopicItem("To Vote or Not to Vote"))
-        items.add(TopicItem("Peanut Butter is Better than Jelly"))
-        items.add(TopicItem("Covid a Hoax?"))
-        items.add(TopicItem("Football is "))
-
-
-        return items
-    }
 
 }
