d50d7a92b90c12f7b9187c627a907c3cb1712c43
Michael Akinyemi <michael@outstrip.biz>
2020-12-06 20:48:27 -0500

    Merged Topics branch with Home

--- Project/app/src/main/java/cmsc436/changemyview/MainActivity.kt
+++ Project/app/src/main/java/cmsc436/changemyview/MainActivity.kt
@@ -1,11 +1,51 @@
 package cmsc436.changemyview
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var debateButton : Button
+    private lateinit var observerButton : Button
+    private lateinit var logoutButton : Button
+    private lateinit var profileButton : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home)
+
+        debateButton = findViewById(R.id.home_btn_debate)
+        observerButton = findViewById(R.id.home_btn_observe)
+        logoutButton = findViewById(R.id.home_btn_logout)
+        profileButton = findViewById(R.id.home_btn_profile)
+
+        debateButton.setOnClickListener {
+            val intent = Intent(this, TopicActivity::class.java)
+            //TODO
+            //Set flag user in database as a debater
+            startActivity(intent)
+        }
+
+        observerButton.setOnClickListener{
+            val intent = Intent(this, TopicActivity::class.java)
+            //TODO
+            //Set flag user in database as a observer
+            startActivity(intent)
+        }
+
+        logoutButton.setOnClickListener {
+            //TODO
+            //log user out and then go back to login activity
+
+        }
+
+        profileButton.setOnClickListener{
+            val intent = Intent(this, TopicActivity::class.java)
+            //TODO
+            //Set flag user in database as a observer
+            startActivity(intent)
+        }
+
     }
 }
---
+++ Project/app/src/main/java/cmsc436/changemyview/ProfileActivity.kt
@@ -0,0 +1,13 @@
+package cmsc436.changemyview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.profile)
+
+    }
+
+}
