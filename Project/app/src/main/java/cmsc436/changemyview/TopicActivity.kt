aa415a7705d5fdb08f99edda7905145ab29e3be9
Michael Akinyemi <michael@outstrip.biz>
2020-12-06 19:21:34 -0500

    topics view done

--- Project/app/src/main/java/cmsc436/changemyview/TopicActivity.kt
+++ Project/app/src/main/java/cmsc436/changemyview/TopicActivity.kt
@@ -1,4 +1,65 @@
 package cmsc436.changemyview
 
-class TopicActivity {
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import cmsc436.changemyview.adapters.TopicItemAdapters
+import cmsc436.changemyview.model.TopicItem
+import com.google.firebase.database.*
+
+class TopicActivity : AppCompatActivity() {
+    private var recyclerView: RecyclerView? = null
+    private var gridLayoutManager: GridLayoutManager? = null
+    private var arrayList:ArrayList<TopicItem> ? = null
+    private var topicItemAdapters: TopicItemAdapters? = null
+    private lateinit var database:FirebaseDatabase
+    private lateinit var reference:DatabaseReference
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.topics_fragment)
+
+        recyclerView = findViewById(R.id.my_topic_recycler_view)
+        gridLayoutManager =
+            GridLayoutManager(applicationContext, 2, GridLayoutManager.VERTICAL, false)
+        recyclerView?.layoutManager = gridLayoutManager
+        recyclerView?.setHasFixedSize(true)
+
+        arrayList = ArrayList()
+        arrayList = setTopicsToList()
+        topicItemAdapters = TopicItemAdapters(applicationContext, arrayList!!)
+        recyclerView?.adapter = topicItemAdapters
+    }
+
+    private fun setTopicsToList() : ArrayList<TopicItem>{
+
+        var items:ArrayList<TopicItem> = ArrayList()
+        database = FirebaseDatabase.getInstance()
+        reference = database.getReference("/debates")
+
+        reference.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                p0.children.forEach {
+                    Log.d("NewMessage", it.toString())
+                }
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+        items.add(TopicItem("To Vote or Not to Vote"))
+        items.add(TopicItem("Peanut Butter is Better than Jelly"))
+        items.add(TopicItem("Covid a Hoax?"))
+        items.add(TopicItem("Football is "))
+        items.add(TopicItem("Peanut Butter is Better than Jelly"))
+        items.add(TopicItem("Covid a Hoax?"))
+
+        return items
+    }
+
 }
--- Project/app/src/main/java/cmsc436/changemyview/adapters/TopicItemAdapters.kt
+++ Project/app/src/main/java/cmsc436/changemyview/adapters/TopicItemAdapters.kt
@@ -1,4 +1,40 @@
 package cmsc436.changemyview.adapters
 
-class TopicItemAdapters {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import cmsc436.changemyview.R
+import cmsc436.changemyview.model.TopicItem
+
+class TopicItemAdapters(var context: Context, var arrayList: ArrayList<TopicItem>) :
+    RecyclerView.Adapter<TopicItemAdapters.ItemHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
+        val itemHolder = LayoutInflater.from(parent.context)
+            .inflate(R.layout.topic_grid_layout_list, parent, false)
+
+        return ItemHolder(itemHolder)
+    }
+
+    override fun onBindViewHolder(holder: ItemHolder, position: Int) {
+        var topicItem:TopicItem = arrayList[position]
+
+        holder.button!!.text = topicItem.title
+
+        holder.button!!.setOnClickListener {
+            Toast.makeText(context, topicItem.title, Toast.LENGTH_LONG)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return arrayList.size
+    }
+
+    class ItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var button: TextView? = itemView.findViewById<TextView>(R.id.topic1)
+    }
 }
